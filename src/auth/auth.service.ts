98ae8f403fcb2f063e614f5f596d138a929290f4
Artyom Svetilov <your.email@example.com>
2025-10-06 23:22:19 +0300

    added Swagger, docker-compose

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt'
 import { PrismaService } from 'src/prisma/prisma.service';
+import { RegisterDto } from './dto/register.dto';
 
 @Injectable()
 export class AuthService {
@@ -8,4 +9,8 @@ export class AuthService {
         private readonly prisma: PrismaService,
         private readonly jwt: JwtService
     ) {}
+
+    register(registerUserDto: RegisterDto) {
+        return 'Register a new user!'
+    }
 }
