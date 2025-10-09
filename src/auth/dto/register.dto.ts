27ed2d22f7b2f15cb644e198958e2399177f1f28
Artyom Svetilov <your.email@example.com>
2025-10-09 21:22:27 +0300

    added @Matches for password in RegisterDto

--- src/auth/dto/register.dto.ts
+++ src/auth/dto/register.dto.ts
@@ -1,4 +1,4 @@
-import { IsBoolean, IsEmail, IsOptional, IsString, MaxLength, MinLength } from "class-validator";
+import { IsBoolean, IsEmail, IsOptional, IsString, Matches, MaxLength, MinLength } from "class-validator";
 
 export class RegisterDto {
     @IsEmail()
@@ -7,34 +7,40 @@ export class RegisterDto {
     @IsString()
     @MinLength(6)
     @MaxLength(100)
+    @Matches(/^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?])[A-Za-z\d!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]{6,100}$/, 
+        {
+          message:
+            'Пароль должен содержать хотя бы одну заглавную букву, одну цифру, один специальный символ и состоять только из латинских букв и допустимых символов',
+        }
+    )        
     password: string
 
-    @IsString()
-    @MinLength(1)
-    @MaxLength(50)
-    firstName: string
+    @IsString({ message: 'Имя должно быть строкой' })
+    @MinLength(1, { message: 'Имя обязательно для заполнения' })
+    @MaxLength(50, { message: 'Имя слишком длинное' })
+    firstName: string;
 
-    @IsString()
-    @MinLength(1)
-    @MaxLength(50)
-    lastName: string
-
-    @IsOptional()
-    @IsBoolean()
-    is2faEnabled?: boolean
+    @IsString({ message: 'Фамилия должна быть строкой' })
+    @MinLength(1, { message: 'Фамилия обязательна для заполнения' })
+    @MaxLength(50, { message: 'Фамилия слишком длинная' })
+    lastName: string;
 
     @IsOptional()
-    @IsString()
-    @MaxLength(100)
-    otpSecret?: string
+    @IsBoolean({ message: 'is2faEnabled должен быть булевым значением' })
+    is2faEnabled?: boolean;
 
     @IsOptional()
-    @IsString()
-    @MaxLength(255)
-    avatarUrl?: string
+    @IsString({ message: 'otpSecret должен быть строкой' })
+    @MaxLength(100, { message: 'otpSecret слишком длинный' })
+    otpSecret?: string;
 
-    @IsString()
-    @MinLength(30)
-    @MaxLength(1500)
-    bio: string
+    @IsOptional()
+    @IsString({ message: 'avatarUrl должен быть строкой' })
+    @MaxLength(255, { message: 'avatarUrl слишком длинный' })
+    avatarUrl?: string;
+
+    @IsString({ message: 'Биография должна быть строкой' })
+    @MinLength(30, { message: 'Биография должна содержать минимум 30 символов' })
+    @MaxLength(1500, { message: 'Биография слишком длинная' })
+    bio: string;
 }
