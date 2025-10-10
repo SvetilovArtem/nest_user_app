fd610b42cb46d1f5652a89a903ae9c5d4e8ee030
Artyom Svetilov <your.email@example.com>
2025-10-10 14:18:58 +0300

    added RedisModule and RedisService

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,12 +3,14 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { JwtModule } from '@nestjs/jwt';
+import { RedisModule } from 'src/redis/redis.module';
 
 @Module({
   providers: [AuthService],
   controllers: [AuthController],
   imports: [
     PrismaModule,
+    RedisModule,
     JwtModule.register({
       secret: process.env.JWT_SECRET || "your-secret-key",
       signOptions: { expiresIn: "1h" }
